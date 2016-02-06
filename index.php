c5064e4a2ee7baac4eda400c62b7b745e21f38cc
claudito <megabyte1507@gmail.com>
2016-02-06 18:34:40 -0500

    first coommit
    Primera subida

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Perú Tec</title>
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="css/estilos.css">
+	<link rel="shortcut icon" type="image/x-icon" href="imagen/kakashi.png">
+</head>
+<body>
+	
+<div class="container-fluid">
+<div class="row">
+<div class="col-md-12">
+<center>
+ <img src="imagen/kakashi.png" alt="kakashi"
+	 class="img-responsive">
+</center>
+
+</div>
+</div>
+</div>
+</body>
+</html>
